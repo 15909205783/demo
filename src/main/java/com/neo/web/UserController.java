1d87ad5eeb848a52736df443c13db788c492d18e
yangfan <2585928225@qq.com>
2020-05-30 23:51:26 +0800

    1、mybatis二级缓存

--- src/main/java/com/neo/web/UserController.java
+++ src/main/java/com/neo/web/UserController.java
@@ -1,9 +1,10 @@
 package com.neo.web;
 
-import com.neo.goodskill.service.SkillService;
+import com.neo.enums.UserSexEnum;
 import com.neo.mapper.UserMapper;
 import com.neo.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,15 +15,24 @@ import java.util.List;
 public class UserController {
     @Autowired
     private UserMapper userMapper;
+
     @RequestMapping("/getUsers")
     public List<User> getUsers() {
-        List<User> users=userMapper.getAll();
+        List<User> users = userMapper.getAll();
         return users;
+    }
+
+    @RequestMapping("/get")
+    @Cacheable(value = "user-key")
+    public User getUser() {
+        User user = new User("aa", "aa123456", UserSexEnum.WOMAN);
+        System.out.println("若下面没出现“无缓存的时候调用”字样且能打印出数据表示测试成功");
+        return user;
     }
 
     @RequestMapping("/getUser")
     public User getUser(Long id) {
-        User user=userMapper.getOne(id);
+        User user = userMapper.getOne(id);
         return user;
     }
 
@@ -31,12 +41,12 @@ public class UserController {
         userMapper.insert(user);
     }
 
-    @RequestMapping(value="update")
+    @RequestMapping(value = "update")
     public void update(User user) {
         userMapper.update(user);
     }
 
-    @RequestMapping(value="/delete/{id}")
+    @RequestMapping(value = "/delete/{id}")
     public void delete(@PathVariable("id") Long id) {
         userMapper.delete(id);
     }
--- src/test/java/com/neo/redis/TestRedis.java
+++ src/test/java/com/neo/redis/TestRedis.java
@@ -1,5 +1,7 @@
 package com.neo.redis;
 
+import com.neo.enums.UserSexEnum;
+import com.neo.model.User;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.TimeUnit;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -22,5 +27,20 @@ public class TestRedis {
     public void test() throws Exception {
         stringRedisTemplate.opsForValue().set("aaa", "111");
         Assert.assertEquals("111", stringRedisTemplate.opsForValue().get("aaa"));
+    }
+    @Test
+    public void testObj() throws Exception{
+        User user = new User("aa","aa123456", UserSexEnum.WOMAN);
+        ValueOperations<String,User> operations = redisTemplate.opsForValue();
+        operations.set("com.neox",user);
+        operations.set("com.neo.f",user,1, TimeUnit.SECONDS);
+        Thread.sleep(1000);
+        boolean exits = redisTemplate.hasKey("com.neo.f");
+        if (exits){
+            System.out.println("esits is true");
+        }else {
+            System.out.println("exists is false");
+        }
+
     }
 }
