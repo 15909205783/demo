ac00437b51f647203b58c896a92e26a3a1628cc5
yangwuhai <yangwuhai@codemao.cn>
2021-07-04 23:53:00 +0800

    1、数据结构与算法

---
+++ src/main/java/com/neo/struct/CirCleList.java
@@ -0,0 +1,12 @@
+package com.neo.struct;
+
+/**
+ * <p>
+ * Description: 环形链表
+ * </p>
+ *
+ * @author yangwuhai
+ * @since 2021-07-04
+ */
+public class CirCleList {
+}
---
+++ src/main/java/com/neo/struct/Fibonacci.java
@@ -0,0 +1,25 @@
+package com.neo.struct;
+
+/**
+ * <p>
+ * Description:
+ * </p>
+ *
+ * @author yangwuhai
+ * @since 2021-07-04
+ */
+public class Fibonacci {
+    public static void main(String[] args) {
+        System.out.println(calculate(10));
+    }
+
+    public static int calculate(int num) {
+        if (num == 0) {
+            return 0;
+        }
+        if (num == 1) {
+            return 1;
+        }
+        return calculate(num - 1) + calculate(num - 2);
+    }
+}
